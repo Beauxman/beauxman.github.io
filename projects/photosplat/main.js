ed0cbcf53c9954771f3e5716c6ffede116e129a5
Andrew Bowman <76933735+Beauxman@users.noreply.github.com>
2024-09-19 16:33:56 -0700

    Import fixes

--- projects/photosplat/main.js
+++ projects/photosplat/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.168.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.168.0/examples/jsm/loaders/GLTFLoader.js';
 
 // Renderer
 
